3935dbe73b64e39bcdb6c22a4482266f7a569789
Kazushi Yamanishi <keironia187149@gmail.com>
2019-03-28 10:34:37 +0900

    Update rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_08_081146) do
+ActiveRecord::Schema.define(version: 2018_12_23_074819) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
